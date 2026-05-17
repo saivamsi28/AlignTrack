edb9f328b2bf971f80c9b8c155643c6a92d9eb87
saivamsi28 <saivamsi.madhavarpu@gmail.com>
2026-05-17 23:52:04 +0530

    vercel update

--- AlignTrack/src/components/CheckInBoard.tsx
+++ AlignTrack/src/components/CheckInBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import type { Goal, Role, Cycle, AuditLog } from '../types/index';
 
 interface CheckInBoardProps {
--- AlignTrack/src/components/GoalList.tsx
+++ AlignTrack/src/components/GoalList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import type { Goal, Role, SheetStatus } from '../types/index';
 
 interface GoalListProps {
